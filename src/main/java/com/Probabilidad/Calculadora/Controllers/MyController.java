c3569b40f739cf85072a3c566635b12dcf181a3f
Peezma <ezemartinex@gmail.com>
2022-11-15 17:07:51 -0300

    template hipergeometrica alfa

--- src/main/java/com/Probabilidad/Calculadora/Controllers/MyController.java
+++ src/main/java/com/Probabilidad/Calculadora/Controllers/MyController.java
@@ -1,14 +1,19 @@
-package com.Probabilidad.Calculadora;
+package com.Probabilidad.Calculadora.Controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class MyController {
     
     @RequestMapping (value = "index")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping (value = "hipergeometrica")
+    public String Hipergeometrica(){
+        return "hipergeometrica";
     }
 
 }
---
+++ src/main/resources/static/js/hipergeometrica.js
@@ -0,0 +1,3 @@
+function calcularhg(){
+    document.getElementById("probabilidadhg").textContent = "24";
+}
